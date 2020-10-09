60ec0307349b300f171159fb6ca8243193d9ceaa
hankei6km <hankei6km+git@gmail.com>
2020-10-09 21:01:30 +0900

    コメント更新

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     // シークレット関連はここに定義せずに、デプロイ環境などで定義すること
     // 利用するときは getStaticProps などのビルド時に使われる関数か、
     // API Routes にすること(デプロイされたファイルに含まれないコードでのみ利用という意味) .
+    // TODO: 変数未定義のエラー対応
     //
     // BLOG_API_URL_BASE: 'url'
     // BLOG_API_KEY: 'secret-key'
