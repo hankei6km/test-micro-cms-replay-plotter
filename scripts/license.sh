aa1d4642a50b5dfcf3c65e49e2b6b2caaeda7632
hankei6km <hankei6km+git@gmail.com>
2020-10-10 19:55:45 +0900

    改行を調整

--- scripts/license.sh
+++ scripts/license.sh
@@ -3,7 +3,11 @@
 LICENSES_TXT="public/opensource_licenses.txt"
 
 cat LICENSE > "${LICENSES_TXT}"
+echo "" >> "${LICENSES_TXT}"
 echo "---" >> "${LICENSES_TXT}"
+echo "" >> "${LICENSES_TXT}"
 cat LICENSE_next_learn_starter >> "${LICENSES_TXT}"
+echo "" >> "${LICENSES_TXT}"
 echo "---" >> "${LICENSES_TXT}"
+echo "" >> "${LICENSES_TXT}"
 NODE_ENV=production yarn --silent licenses generate-disclaimer >> "${LICENSES_TXT}"
