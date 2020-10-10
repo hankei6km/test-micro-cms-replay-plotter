1e0e18a3b19d0c978678b302f4017a5468fb6639
hankei6km <hankei6km+git@gmail.com>
2020-10-10 19:41:55 +0900

    プレビュー機能 (#11)
    * fallback を true
    デプロイされた場合 falseだと404
    
    * プレビュー時は[DRAFT]を追加
    
    * プレビューを抜ける

---
+++ pages/api/blog_preview.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch';
+
+// https://microcms.io/blog/nextjs-preview-mode
+
+export default async (req, res) => {
+  if (!req.query.slug) {
+    return res.status(404).end();
+  }
+  try {
+    const q = new URLSearchParams('');
+    q.append('fields', 'id');
+    q.append('draftKey', req.query.draftKey);
+    const fres = await fetch(
+      `${process.env.BLOG_API_URL_BASE}/${req.query.slug}?${q.toString()}`,
+      {
+        method: 'GET',
+        headers: { 'X-API-KEY': process.env.BLOG_API_KEY }
+      }
+    );
+    if (fres.ok) {
+      const content = await fres.json();
+      res.setPreviewData({
+        slug: content.id,
+        draftKey: req.query.draftKey
+      });
+      res.writeHead(307, { Location: `/posts/${content.id}` });
+      return res.end('Preview mode enabled');
+    } else {
+      return res.status(401).json({ message: 'Invalid slug' });
+    }
+  } catch (err) {
+    return res.status(401).json({ message: res.statusText });
+  }
+};
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,37 @@
+// import { useRouter } from 'next/router';
+import Head from 'next/head';
+import ErrorPage from 'next/error';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
-import Head from 'next/head';
 import Date from '../../components/date';
 import Plotter from '../../components/plotter';
 import utilStyles from '../../styles/utils.module.css';
 
-export default function Post({ postData }) {
+export default function Post({ postData, preview }) {
+  // https://github.com/vercel/next.js/blob/b41f9baaa413d5dac29faf107663214c0923c8bd/examples/cms-contentful/pages/posts/%5Bslug%5D.js
+  // const router = useRouter();
+
+  // if (!router.isFallback && !postData) {
+  if (!postData) {
+    // cookie がないときに `An unexpected error has occurred.' となる
+    // これが原因かはわからないが
+    return <ErrorPage statusCode={404} />;
+  }
+
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className={utilStyles.headingXl}>
+          {preview && (
+            <a href="/api/blog_exit_preview" className={utilStyles.draftText}>
+              {'[DRAFT(click to exit)] '}
+            </a>
+          )}
+          {postData.title}
+        </h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
@@ -29,15 +48,16 @@ export async function getStaticPaths() {
   const paths = await getAllPostIds();
   return {
     paths,
-    fallback: false
+    fallback: true
   };
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export async function getStaticProps(context) {
+  const postData = await getPostData(context);
   return {
     props: {
-      postData
+      postData,
+      preview: context.preview ? context.preview : null
     }
   };
 }
